9e72e49524160eecb3dbc9da5ce61117130cef08
théo MALKA--LACOMBE <theomalkalacombe@MacBook-Pro-de-theo-2.local>
2021-02-25 18:44:41 +0100

    bar chart added - connecter le dynamique

--- js/barChart.js
+++ js/barChart.js
@@ -4,31 +4,33 @@ function drawBarChart(
     width,
     height,
     title,
-    streamingHistory
-){ 
-
+    dailyListeningTime
+){         
+    dailyListeningTimeFiltered = dailyListeningTime.filter(d => {
+        return d["month"] == "2020-12"
+      })
     d3.select(container).selectAll("*").remove();
 
     const svg = d3
-        .select(container)
-        .append("svg")
-        .attr("viewBox", [
+    .select(container)
+    .append("svg")
+    .attr("viewBox", [
         0,
         0,
         width + margin.left + margin.right,
         height + margin.top + margin.bottom,
-        ]);
-    const svg = d3.create("svg").attr("width", width + 100).attr("height", height + 100)
-  
+      ]);
+    
+    
     const g = svg.append("g").attr("transform", "translate(80 0)")
-  
+    
     const x = d3.scaleBand().domain(Array.from(dailyListeningTimeFiltered).map(d => d["day"])).range([0, width]).padding(.2)
-  
+    
     const y = d3.scaleLinear().domain([0, d3.max(dailyListeningTimeFiltered, d => d["totalPlayedTimeMin"])]).range([height, 0])
-  
+    
     const yAxis = g => g
         .call(d3.axisLeft(y))
-  
+    
     const rect = g.selectAll("rect")
         .data(dailyListeningTimeFiltered)
         .enter()
@@ -38,7 +40,7 @@ function drawBarChart(
         .attr("width", d => x.bandwidth())
         .attr("height",  d => y(d["totalPlayedTimeMin"]))
         .style("fill", "steelblue")
-  
+    
     const text = g.selectAll("text")
         .data(dailyListeningTimeFiltered)
         .enter()
@@ -47,15 +49,27 @@ function drawBarChart(
         .attr("y", d => width)
         .attr("transform", d => "translate(" + x.bandwidth()/4 + " " + 10 + ") rotate(45 " + x(d["day"]) + ", " + height + ")")
         .text(d => d["day"])
-  
+    
     const titre = g
         .append("text")
-        .attr("x", width/4)
-        .attr("y", height + 60)
+        .attr("x", width/4 + 10)
+        .attr("y", height + 80)
         .text(title)
-  
+    
     g.append("g")
         .call(yAxis);
+    
+    
+    function filterMonth(selectedMonth){
+        let dailyListeningTimefiltered = dailyListeningTime.filter(d => {
+            return d["month"] == selectedMonth
+        })
+        return dailyListeningTimefiltered
+    }
   
-  return svg.node()
+    return Object.assign(svg.node(), {
+        filterMonth: filterMonth,
+      });
+
+
 }
